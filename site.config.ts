fba786366007dfd159fb8eae7f5c303859bf65f7
neeson <neesond@foxmail.com>
2023-01-29 10:50:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   //twitter: '',
-  github: 'neeson',
+  github: 'neesond',
   //linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
